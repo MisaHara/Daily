6c82082d3b46cda56358da4737fd1c8e4658bf7a
Misa <rudy-2000w@mail.ru>
2025-02-03 23:21:24 +0600

    First

---
+++ src/main/java/com/Daily/DailyApp/DailyAppApplication.java
@@ -0,0 +1,13 @@
+package com.Daily.DailyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyAppApplication.class, args);
+	}
+
+}
